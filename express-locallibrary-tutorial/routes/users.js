215612a61cfa559345aeb418ca4408a72fffa0a1
Mateus da Rosa Elias <mateuselias4048@gmail.com>
2023-09-14 13:27:18 -0300

    Mongo

---
+++ express-locallibrary-tutorial/routes/users.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/ifc', function(req, res, next) {
+  res.send('BEM VINDO!!.......................................(ao seu maior sonho e pesadelo)');
+});
+
+router.get('/cool', function(req, res, next) {
+  res.send('Legal');
+});
+
+router.get('/cool/beach', function(req, res, next) {
+  res.send('Praia é uma maravilha, quando é as férias mesmo?');
+});
+
+module.exports = router;
